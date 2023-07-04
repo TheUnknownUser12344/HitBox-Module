60d7cff480a926b6561c1c01093aa1f84af20dc3
TheUnknownUser12344 <128866149+TheUnknownUser12344@users.noreply.github.com>
2023-07-04 15:14:32 -0700

    Update mainHitBox.lua

--- mainHitBox.lua
+++ mainHitBox.lua
@@ -2,29 +2,39 @@ local HitBox = {
 	OverlapParameters = nil,
 }
 
+--Single Inheritance
 HitBox.__index = HitBox
 local Players = game:GetService("Players")
 
-local function CreateHitBox(self)
+local function CreateHitBox(self):{}
 	local size
 	local ToolCFrame
 
+	--Setting OverlapParameters To Filter
 	if not self.OverlapParameters then
 		self.OverlapParameters = OverlapParams.new()
 		self.OverlapParameters.FilterDescendantsInstances = {self._tool}
 		self.OverlapParameters.FilterType = Enum.RaycastFilterType[self._FilterType]
 	end
-
-	if self._tool:IsA("Model") then size = self._tool:GetExtentsSize() ToolCFrame = self._tool:GetPivot() else size = self._tool.Size ToolCFrame = self._tool.CFrame end
-
+	
+	--Switch Methods Based On Class
+	if self._tool:IsA("Model") then 
+		size = self._tool:GetExtentsSize() ToolCFrame = self._tool:GetPivot() 
+	else 
+		size = self._tool.Size ToolCFrame = self._tool.CFrame 
+	end
+	
+	--Create A Box To Handle Hit Detection
 	local GetPartsInBox = workspace:GetPartBoundsInBox(ToolCFrame, size, self.OverlapParameters)	
 
 	return GetPartsInBox
 end
 
-local function CheckFilteredTable(self, Humanoid, instance)
-	local SetFilter = false
+--Identify If A Object Has Been Hit
 
+local function CheckFilteredTable(self, Humanoid, instance):boolean
+	local SetFilter = false
+	
 	for _, filteredParts in self._FilterTable do
 		if Humanoid then
 			if Humanoid == filteredParts or instance == filteredParts then
@@ -42,29 +52,37 @@ local function CheckFilteredTable(self, Humanoid, instance)
 	return SetFilter
 end
 
-local function FindAllHumanoids(self)
+local function FindAllHumanoids(self):{}
 	local Clear = false
 	local GetPartsInBox = CreateHitBox(self)
 
 	local AllHumanoids = {}
-
+	
+	--Loops Through Hit Objects
 	for _, instance in GetPartsInBox do
 		local Humanoid = instance.Parent:FindFirstChild("Humanoid")
-
+		
+		--If Its Already Been Found Then Skip		
+		
 		if CheckFilteredTable(self, Humanoid, instance) then continue end
-
+		
+		--If There Is No Humanoid Then Skip
+		
 		if not Humanoid then continue end  
-
+		
 		if #AllHumanoids == 0 then 
 			table.insert(AllHumanoids, Humanoid)
 		else
-			for _, humanoids in AllHumanoids do
+			for _, humanoids in AllHumanoids do --Loop Through All Humanoids Found
 				if humanoids ~= Humanoid then continue end
+				
+				--If It Has Found It Then Dont Insert The Humanoid
 				Clear = true
 				break
 			end
 
 			if not Clear then
+				--If Not Found The Insert It Into The Humanoids Table
 				table.insert(AllHumanoids, Humanoid)
 			end
 
@@ -75,12 +93,8 @@ local function FindAllHumanoids(self)
 	return AllHumanoids
 end		
 
-function HitBox.new(tool, Size, BoxCFrame)
-	if not tool then	if not Size and not BoxCFrame then return end end
-
-	local size = tool or Size
-	local ToolCFrame = tool or BoxCFrame
-
+--Create A Constructer For The Hitbox
+function HitBox.new(tool)
 	local metaTable = setmetatable({
 		_canAttack = true,
 		_tool = tool,
@@ -92,7 +106,8 @@ function HitBox.new(tool, Size, BoxCFrame)
 	return metaTable
 end
 
-function HitBox:GetTouchingParts()
+function HitBox:GetTouchingParts():{}
+	--Return EveryPart The Hitbox Has Interacted With
 	return CreateHitBox(self)
 end
 
@@ -100,19 +115,22 @@ function HitBox:OnHit()
 	local ContactParts = CreateHitBox(self)
 	local AllHumanoids = FindAllHumanoids(self)
 	
+	--Fire A BindableEvent When The Hitbox Has Hit Something
 	if AllHumanoids or ContactParts then
-		pcall(function()
-			self.OnHitEvent:Fire(ContactParts, AllHumanoids)
-		end)
+		self.OnHitEvent:Fire(ContactParts, AllHumanoids)
 	end
 end
 
-function HitBox:SetFilterType(FilterType)
+function HitBox:SetFilterType(FilterType : string)
+	--Set the FilterType For The Hitbox
 	self._FilterType = FilterType
 end
 
-function HitBox:GetAllHumanoids(WaitForDelay)
+function HitBox:GetAllHumanoids(WaitForDelay : boolean):{}
+	--Return All Humanoids	
+	
 	if WaitForDelay then
+		--Add A Optional Delay
 		repeat task.wait() until self._canAttack 
 
 		return FindAllHumanoids(self)
@@ -121,40 +139,45 @@ function HitBox:GetAllHumanoids(WaitForDelay)
 	end
 end
 
-function HitBox:GetFirstHumanoid(WaitForDelay)
+function HitBox:GetFirstHumanoid(WaitForDelay):Humanoid
+	--Get The First Humanoid That Was Found
 	local GetPartsInBox = CreateHitBox(self)
-
+	
 	local function GetHumanoid()
 		for _, instance in GetPartsInBox do
 			local Humanoid = instance.Parent:FindFirstChild("Humanoid")
+			--Look For A Humanoid That Has Not Been Found
 			if CheckFilteredTable(self, Humanoid, instance) then continue end
 			if Humanoid then return Humanoid end
 		end
 	end
-
+	
 	if WaitForDelay then
 		repeat task.wait() until self._canAttack
-
+		--Add A Optional Delay
 		return GetHumanoid()
 	else
 		return GetHumanoid()	
 	end
 end
 
-function HitBox:AddDelay(DelayTime, func)
+function HitBox:AddDelay(DelayTime : number, func)
+	--Delay Function To Handle The Hitbox Delays
 	task.spawn(function()
 		self._canAttack = false
 		task.wait(DelayTime)
 		self._canAttack = true
+		--After The Delay Has Finished Optionally Call A Function
 		if func then func() end
 	end)	
 end
 
-function HitBox:FindSpecificClass(Class)
+function HitBox:FindSpecificClass(Class : string)
+	--Look For A Specific Class Only
 	local GetPartInBox = CreateHitBox(self)
 	local AllSpecificInstances = {}
 	local FirstInstance
-
+	
 	for _, instance in GetPartInBox do
 		if instance.ClassName ~= Class then continue end
 		if not FirstInstance then FirstInstance = instance end
@@ -165,13 +188,16 @@ function HitBox:FindSpecificClass(Class)
 end
 
 function HitBox:AddToFilter(instance)
+	--Add To FilterTable So The Hitbox Does Not Pick These Up
 	table.insert(self._FilterTable, instance)
 end
 
-function HitBox:RemoveFromFilter(instance, instanceIndex)
+function HitBox:RemoveFromFilter(instance : Instance, instanceIndex : number?)
+	--Remove Instance From The Filter Table
 	task.spawn(function()
 		for index, filteredInstance in self._FilterTable do
-			if instance == filteredInstance or instanceIndex == index then 
+			if instance == filteredInstance or instanceIndex == index then
+				--If the Instance Or InstanceIndex Is In The Table Then Remove It
 				table.remove(self._FilterTable, index)
 				break
 			end
@@ -179,21 +205,22 @@ function HitBox:RemoveFromFilter(instance, instanceIndex)
 	end)
 end
 
-function HitBox:GetFilterTable()
+function HitBox:GetFilterTable():{}
+	--Return The FilterTable
 	return self._FilterTable
 end
 
-function HitBox:CheckFilters(humanoids)
+function HitBox:CheckFilters(instance): boolean
 	local clear = false
-
+	
+	--Check If A Object Is In The Filter Table
 	for i, v in self._FilterTable do
-		if v == humanoids then
+		if v == instance then
 			clear = true
 		end
 	end
 
 	return clear
 end
-
 
 return HitBox
